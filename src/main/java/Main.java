5e3212a342fdb0435409a0685f8205a9a50d8094
Ingvard <ingvard777333@gmail.com>
2026-02-08 22:06:20 +0300

    TwoTest

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 VacationService service = new VacationService();
+/*
 int income = 10_000;
 int expenses = 3_000;
 int threshold = 20_000;
@@ -12,5 +13,7 @@ int amount = 0;
 
 int web = service.calc(income, expenses, threshold, amount);
         System.out.println(web);
+
+ */
     }
 }
--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -6,11 +6,22 @@ import org.junit.jupiter.api.Test;
 public class VacationServiceTest {
 
     @Test
-    public void calcVacation() {
+    public void calcVacationincome100() {
         VacationService service = new VacationService();
         int income = 100_000;
         int expenses = 60_000;
         int threshold = 150_000;
+        int amount = 0;
+        int boss = service.calc(income, expenses, threshold, amount);
+        System.out.println(boss);
+    }
+
+    @Test
+    public void calcVacationincome10() {
+        VacationService service = new VacationService();
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
         int amount = 0;
         int boss = service.calc(income, expenses, threshold, amount);
         System.out.println(boss);
