6c2196ba3efe17a4faae8b2390be550b84a09cd1
Ingvard <ingvard777333@gmail.com>
2026-02-08 22:44:38 +0300

    CSVinside

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -1,29 +1,37 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 //import ru.netology.services.VacationService;
 
 public class VacationServiceTest {
 
-    @Test
-    public void calcVacationincome100() {
+    @ParameterizedTest
+    @CsvSource({
+            "10000,3000,20000,0",
+            "100000,60000,150000,0"
+    })
+    public void calcVacationincome(int income, int expenses, int threshold, int amount) {
         VacationService service = new VacationService();
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int amount = 0;
+//        int income = 100_000;
+//        int expenses = 60_000;
+//        int threshold = 150_000;
+//        int amount = 0;
         int boss = service.calc(income, expenses, threshold, amount);
         System.out.println(boss);
     }
 
-    @Test
-    public void calcVacationincome10() {
-        VacationService service = new VacationService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int amount = 0;
-        int boss = service.calc(income, expenses, threshold, amount);
-        System.out.println(boss);
-    }
+//    @Test
+//    public void calcVacationincome10() {
+//        VacationService service = new VacationService();
+//        int income = 10_000;
+//        int expenses = 3_000;
+//        int threshold = 20_000;
+//        int amount = 0;
+//        int boss = service.calc(income, expenses, threshold, amount);
+//        System.out.println(boss);
+//    }
+//
+//
 }
